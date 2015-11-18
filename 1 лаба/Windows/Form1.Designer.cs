c2e9f451a787932acd8465fd6979c7ba56dfbfcc
Gleb E. Vlaskin <glebvlaskin@tpu.ru>
2015-11-18 09:52:01 +0600

    Добавлена button1

--- 1 лаба/Windows/Form1.Designer.cs
+++ 1 лаба/Windows/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Посмотреть таблицу клиентов";
+            this.button1.Text = "Таблица клиентов";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
